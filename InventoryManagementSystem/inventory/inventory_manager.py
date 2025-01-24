694fbc5a4d504ddf1fec9d6cb443d81bd8a08d61
Philip Koloczek <koloczek@gmail.com>
2025-01-24 11:04:56 +0100

    json functionality

--- InventoryManagementSystem/inventory/inventory_manager.py
+++ InventoryManagementSystem/inventory/inventory_manager.py
@@ -1,14 +1,17 @@
 import json 
 
 
-
-with open("product.json", "r") as f:
+with open("/home/philip/projects/project-python-basics-inventory-management-system-philip-koloczek-somayeh-bahari/InventoryManagementSystem/inventory/product.json", "r") as f:
     products = json.load(f)
-    
-products["dishwasher"]["price"] = 250
 
+# Änderungen vornehmen
+products["dishwasher"]["price"] = 160
+
+# Ergebnis anzeigen
 print(products["dishwasher"]["price"])
 
+with open("/home/philip/projects/project-python-basics-inventory-management-system-philip-koloczek-somayeh-bahari/InventoryManagementSystem/inventory/product.json", "w") as f:
+    json.dump(products, f, indent=4)
 
 
 print("New price for 'dishwasher':", products["dishwasher"]["price"])
